ad773d5cc60820f50d71bd332f8dd8e0b08be9f2
Simon Støvring <mail@simonbs.dk>
2025-02-18 16:50:26 +0100

    Simplifies examples

--- SwiftUIExample/ExampleSpiceStore.swift
+++ SwiftUIExample/ExampleSpiceStore.swift
@@ -9,19 +9,11 @@ enum ServiceEnvironment: String, CaseIterable {
 final class ExampleSpiceStore: SpiceStore {
     @Spice(requiresRestart: true) var environment: ServiceEnvironment = .production
     @Spice var enableLogging = false
-    let featureFlags = FeatureFlagsSpiceStore()
     @Spice var clearCache = {
         URLCache.shared.removeAllCachedResponses()
     }
-    @Spice var longOperation = {
-        try await Task.sleep(nanoseconds: 1_000_000_000)
-    }
-    @Spice var failingOperation = {
-        try await Task.sleep(nanoseconds: 1_000_000_000)
-        throw NSError(domain: "dk.shape.Spices", code: -1, userInfo: [
-            NSLocalizedDescriptionKey: "🚨 This error was intentional to demonstrate how Spices handles failing operations."
-        ])
-    }
+    
+    let featureFlags = FeatureFlagsSpiceStore()
 }
 
 final class FeatureFlagsSpiceStore: SpiceStore {
--- UIKitExample/ExampleSpiceStore.swift
+++ UIKitExample/ExampleSpiceStore.swift
@@ -11,19 +11,11 @@ final class ExampleSpiceStore: SpiceStore {
 
     @Spice(requiresRestart: true) var environment: ServiceEnvironment = .production
     @Spice var enableLogging = false
-    let featureFlags = FeatureFlagsSpiceStore()
     @Spice var clearCache = {
         URLCache.shared.removeAllCachedResponses()
     }
-    @Spice var longOperation = {
-        try await Task.sleep(nanoseconds: 1_000_000_000)
-    }
-    @Spice var failingOperation = {
-        try await Task.sleep(nanoseconds: 1_000_000_000)
-        throw NSError(domain: "dk.shape.Spices", code: -1, userInfo: [
-            NSLocalizedDescriptionKey: "🚨 This error was intentional to demonstrate how Spices handles failing operations."
-        ])
-    }
+
+    let featureFlags = FeatureFlagsSpiceStore()
 
     private init() {}
 }
