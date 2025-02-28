e15bc8356bff1316a6314cd7e190ee2b56da055c
Harlan Haskins <harlan@harlanhaskins.com>
2025-02-28 12:28:30 -0500

    Better support for initialisms in spice names
    Right now all uppercase characters are being treated as word boundaries, which is sub-optimal.
    It causes property names like "localURL" to become "Local U R L", necessitating overrides
    for all the properties.
    
    This new implementation keeps multiple uppercase characters together as a single word, and splits
    instead on the boundary where an uppercase character is introduced after a lowercase one.
    
    This does a better job of preserving initialisms, but doesn't properly handle initialisms at the start of
    the spice name. I'm not sure a great solution for that, so clients should just override those where appropriate.

---
+++ Tests/SpicesTests/CamelCaseToNaturalTextTests.swift
@@ -0,0 +1,23 @@
+import Foundation
+import Testing
+
+@testable import Spices
+
+@Suite
+struct CamelCaseToNaturalTextTests {
+    @Test
+    func it_handles_simple_cases() async throws {
+        #expect("simpleName".camelCaseToNaturalText() == "Simple Name")
+        #expect("useLocalURL".camelCaseToNaturalText() == "Use Local URL")
+        #expect("environment".camelCaseToNaturalText() == "Environment")
+        #expect("enableLogging".camelCaseToNaturalText() == "Enable Logging")
+        #expect("clearCache".camelCaseToNaturalText() == "Clear Cache")
+        #expect("featureFlags".camelCaseToNaturalText() == "Feature Flags")
+        #expect("notifications".camelCaseToNaturalText() == "Notifications")
+        #expect("fastRefreshWidgets".camelCaseToNaturalText() == "Fast Refresh Widgets")
+
+        // Suboptimal, but heuristics for these would be annoying.
+        #expect("httpVersion".camelCaseToNaturalText() == "Http Version")
+        #expect("apiURL".camelCaseToNaturalText() == "Api URL")
+    }
+}
